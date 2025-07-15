1f815484d439b5384d3ab6fa3841db6be4ecae5f
Юрий Силенок <yuri.silenok@gmail.com>
2025-07-15 12:22:42 +0000

    a

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Для развертывания окружения pip install -e ."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="lern_api",
+    version="0.0.0",
+    packages=find_packages(),
+)
